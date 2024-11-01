9a8b2fe38b0eb96feb97d5050631ef87e83ac26b
Trần Lê Tuấn Anh <anhtltth2304022@fpt.edu.vn>
2024-11-01 20:30:16 +0700

    Login

---
+++ frontend/src/app/login/page.jsx
@@ -0,0 +1,166 @@
+"use client";
+
+import React, { useState } from 'react';
+
+const LoginPage = () => {
+  const [showRecoverForm, setShowRecoverForm] = useState(false);
+
+  // CSS nội tuyến
+  const styles = {
+    wrapper: {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      minHeight: '100vh',
+    },
+    mainContent: {
+      textAlign: 'center',
+      fontFamily: 'Arial, sans-serif',
+    },
+    title: {
+      fontSize: '24px',
+      color: '#50222b', // Đã sửa màu
+      fontWeight: 'bold',
+      marginBottom: '10px', // Đã sửa khoảng cách
+    },
+
+    input: {
+      width: '100%',
+      maxWidth: '400px',
+      padding: '12px',
+      margin: '10px 0',
+      border: '1px solid #5c2a1d',
+      borderRadius: '3px',
+      fontSize: '16px',
+    },
+    button: {
+        backgroundColor: '#000',
+        color: '#fff',
+        padding: '12px 0',
+        width: '100%',
+        padding: '7px 0', // Tăng chiều cao của nút
+        maxWidth: '400px',
+        fontSize: '16px',
+        fontWeight: 'bold',
+        fontStyle: 'italic', // Chữ in nghiêng
+        border: 'none',
+        borderRadius: '3px',
+        cursor: 'pointer',
+        marginTop: '10px',
+      },
+      
+    buttonHover: {
+      backgroundColor: '#333',
+    },
+    link: {
+      color: '#5c2a1d',
+      fontSize: '14px',
+      textDecoration: 'none',
+    },
+    linkHover: {
+      textDecoration: 'underline',
+    },
+    paragraph: {
+      marginTop: '10px',
+    },
+  };
+
+  return (
+    <main style={styles.wrapper}>
+      <div style={styles.mainContent}>
+        <div style={styles.title}>ĐĂNG NHẬP</div>
+
+        {!showRecoverForm ? (
+          <div id="CustomerLoginForm" className="form-vertical">
+            <form acceptCharset="UTF-8" action="/account/login" id="customer_login" method="post">
+              <input name="form_type" type="hidden" value="customer_login" />
+              <input name="utf8" type="hidden" value="✓" />
+
+              <input
+                type="email"
+                name="customer[email]"
+                id="CustomerEmail"
+                placeholder="Email"
+                autoCorrect="off"
+                autoCapitalize="off"
+                autoFocus
+                style={styles.input}
+              />
+
+               <input
+                type="password"
+                name="customer[password]"
+                id="CustomerPassword"
+                placeholder="Mật khẩu"
+                style={styles.input}
+              />
+
+              <p>
+                <input
+                  type="submit"
+                  value="Đăng nhập"
+                  style={styles.button}
+                  onMouseOver={(e) => (e.target.style.backgroundColor = styles.buttonHover.backgroundColor)}
+                  onMouseOut={(e) => (e.target.style.backgroundColor = styles.button.backgroundColor)}
+                />
+              </p>
+              <p style={styles.paragraph}>
+                <a href="#recover" onClick={() => setShowRecoverForm(true)} style={styles.link}>
+                  Quên mật khẩu?
+                </a>
+              </p>
+              <p style={styles.paragraph}>
+              <a href="#" style={{ color: '#5c2a1d', fontSize: '14px', textDecoration: 'none' }}>Bạn chưa có tài khoản </a>
+                <a href="#signup" onClick={() => setShowSignUpForm(true)} style={{ ...styles.link, color: 'blue' }}>
+                  Đăng ký !
+                </a>
+              </p>
+            </form>
+          </div>
+        ) : (
+          <div id="RecoverPasswordForm">
+            <h2>Đặt lại mật khẩu của bạn</h2>
+            <p>Chúng tôi sẽ gửi cho bạn một email để đặt lại mật khẩu của bạn.</p>
+
+            <div className="form-vertical">
+              <form acceptCharset="UTF-8" action="/account/recover" method="post">
+                <input name="form_type" type="hidden" value="recover_customer_password" />
+                <input name="utf8" type="hidden" value="✓" />
+
+                <label htmlFor="RecoverEmail" className="hidden-label">Email</label>
+                <input
+                  type="email"
+                  name="email"
+                  id="RecoverEmail"
+                  placeholder="Email"
+                  autoCorrect="off"
+                  autoCapitalize="off"
+                  style={styles.input}
+                />
+
+                <p>
+                  <input
+                    type="submit"
+                    value="Gửi"
+                    style={styles.button}
+                    onMouseOver={(e) => (e.target.style.backgroundColor = styles.buttonHover.backgroundColor)}
+                    onMouseOut={(e) => (e.target.style.backgroundColor = styles.button.backgroundColor)}
+                  />
+                </p>
+                <button
+                  type="button"
+                  onClick={() => setShowRecoverForm(false)}
+                  style={{ ...styles.link, border: 'none', background: 'none', cursor: 'pointer' }}
+                >
+                  Thoát
+                </button>
+              </form>
+            </div>
+          </div>
+        )}
+      </div>
+    </main>
+  );
+};
+
+export default LoginPage;
--- frontend/src/app/shop/page.jsx
+++ frontend/src/app/shop/page.jsx
@@ -1,6 +1,9 @@
-import React from 'react';
+"use client"; // Thêm dòng này
+
+import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../../css/Shop.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 const Shop = () => {
   const categories = [
@@ -30,6 +33,8 @@ const Shop = () => {
     { title: "Lana Del Rey - Born to Die", price: "370,000", img: "https://via.placeholder.com/150" },
     { title: "Kanye West - Donda", price: "490,000", img: "https://via.placeholder.com/150" }
   ];
+
+  const [hoveredIndex, setHoveredIndex] = useState(null);
 
   const paginationStyle = {
     marginBottom: '1em',
@@ -66,102 +71,160 @@ const Shop = () => {
     <div className="container-fluid">
       {/* Breadcrumb Section */}
       <nav className="breadcrumb" role="navigation" aria-label="breadcrumbs" style={{
-    display: 'flex',
-    justifyContent: 'center', // Căn giữa breadcrumb
-    flexWrap: 'wrap',
-    padding: '0',
-    marginBottom: '1rem',
-    fontSize: '14px',
-    listStyle: 'none',
-    backgroundColor: 'transparent',
-    borderRadius: '0',
-}}>
-    <div className="inner" style={{
         display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        padding: '15px 30px', // Thay đổi padding để làm dài phần nền xám
-        backgroundColor: '#f5f5f5',
-        borderRadius: '4px',
-        width: '100%', // Đảm bảo phần nền chiếm toàn bộ chiều rộng
-        maxWidth: '8000px', // Tùy chọn: Giới hạn chiều rộng tối đa
-    }}>
-        <a href="/" title="Quay lại trang chủ" style={{
+        justifyContent: 'center', // Căn giữa breadcrumb
+        flexWrap: 'wrap',
+        padding: '0',
+        marginBottom: '1rem',
+        fontSize: '14px',
+        listStyle: 'none',
+        backgroundColor: 'transparent',
+        borderRadius: '0',
+      }}>
+        <div className="inner" style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          padding: '15px 30px', // Thay đổi padding để làm dài phần nền xám
+          backgroundColor: '#f5f5f5',
+          borderRadius: '4px',
+          width: '100%', // Đảm bảo phần nền chiếm toàn bộ chiều rộng
+          maxWidth: '8000px', // Tùy chọn: Giới hạn chiều rộng tối đa
+        }}>
+          <a href="/" title="Quay lại trang chủ" style={{
             textDecoration: 'none',
             color: '#a2a2a2',
             fontWeight: 'bold',
             fontSize: '16px',
             fontStyle: 'italic',
-        }}>Trang chủ</a>
-        <span aria-hidden="true" style={{ margin: '0 10px' }}> / </span>
-        <span style={{ fontStyle: 'italic', fontSize: '16px' }}>AVAILABLE NOW</span>
-    </div>
-</nav>
-
+          }}>Trang chủ</a>
+          <span aria-hidden="true" style={{ margin: '0 10px' }}> / </span>
+          <span style={{ fontStyle: 'italic', fontSize: '16px' }}>AVAILABLE NOW</span>
+        </div>
+      </nav>
 
       {/* Image Section */}
       <div className="text-center mb-4">
         <img
           src="https://file.hstatic.net/1000304920/collection/cua_hang_bang_dia_goc_available_now_332f289aeaf346c3a5c9e3f7361a414b_master.png"
           alt="Cửa hàng băng đĩa"
-          style={{ maxWidth: '70%', height: 'auto', margin: '0 auto' }} // Giảm kích thước hình ảnh
+          style={{
+            display: 'block',
+            maxWidth: '1200px',
+            height: 'auto',
+            margin: '0 auto',
+          }}
         />
       </div>
 
       <div className="row">
         {/* Categories Section */}
         <div className="col-md-3" style={{ padding: '45px' }}>
-        <h4 style={{ 
-    fontSize: '24px', 
-    marginBottom: '0', // Để không có khoảng cách giữa tiêu đề và gạch chân
-    color: '#b30000', 
-    fontWeight: 'bold', 
-    borderLeft: '24px solid #d60000', 
-    paddingLeft: '10px'
-}}>
-    Thể Loại
-</h4>
-<div style={{ 
-    width: '100%', 
-    height: '2px', // Độ dày của đường gạch chân
-    backgroundColor: '#b30000', // Màu của đường gạch chân
-    marginTop: '28px', // Khoảng cách giữa tiêu đề và đường gạch chân
-}}></div>
+          <h4 style={{
+            fontSize: '24px',
+            marginBottom: '0', // Để không có khoảng cách giữa tiêu đề và gạch chân
+            color: '#b30000',
+            fontWeight: 'bold',
+            borderLeft: '24px solid #d60000',
+            paddingLeft: '10px'
+          }}>
+            Thể Loại
+          </h4>
+          <div style={{
+            width: '100%',
+            height: '2px', // Độ dày của đường gạch chân
+            backgroundColor: '#b30000', // Màu của đường gạch chân
+            marginTop: '28px', // Khoảng cách giữa tiêu đề và đường gạch chân
+          }}></div>
 
-  <ul className="list-unstyled" style={{ padding: 0 }}>
-    {categories.map((item, index) => (
-      <li key={index} className="d-flex justify-content-between align-items-center mb-1" style={{ borderBottom: '1px solid #b30000', paddingBottom: '5px', fontSize: '12px' }}>
-        <a href="#" style={{ textDecoration: 'none', color: '#333', fontWeight: 'bold', display: 'block', padding: '7px 20px' }}>{item}</a>
-        <span style={{ color: '#b30000', fontWeight: 'bold' }}>15</span>
-      </li>
-    ))}
-  </ul>
+          <ul className="list-unstyled" style={{ padding: 0 }}>
+            {categories.map((item, index) => (
+              <li key={index} className="d-flex justify-content-between align-items-center mb-1" style={{ borderBottom: '1px solid #b30000', paddingBottom: '5px', fontSize: '12px' }}>
+                <a href="#" style={{ textDecoration: 'none', color: '#333', fontWeight: 'bold', display: 'block', padding: '7px 20px' }}>{item}</a>
+                <span style={{ color: '#b30000', fontWeight: 'bold' }}>15</span>
+              </li>
+            ))}
+          </ul>
 
-  <div className="mt-3 text-center">
-    <h4 style={{ fontWeight: 'bold', marginBottom: '10px', fontSize: '14px' }}>NOW PLAYING</h4>
-    <iframe
-      src="https://open.spotify.com/embed/playlist/0lLgC9zfleQhs3l4CI1k8g"
-      width="100%"
-      height="380"
-      frameBorder="0"
-      allow="encrypted-media"
-      title="Now Playing"
-    />
-  </div>
-</div>
-
+          <div className="mt-3 text-center">
+            <h4 style={{ fontWeight: 'bold', marginBottom: '10px', fontSize: '14px' }}>NOW PLAYING</h4>
+            <iframe
+              src="https://open.spotify.com/embed/playlist/0lLgC9zfleQhs3l4CI1k8g"
+              width="100%"
+              height="380"
+              frameBorder="0"
+              allow="encrypted-media"
+              title="Now Playing"
+            />
+          </div>
+        </div>
 
         {/* Products Section */}
         <div className="col-md-9" style={{ padding: '40px' }}>
           <div className="row mt-3">
             {products.map((product, index) => (
-              <div key={index} className="col-md-3 mb-4">
-                <div className="card" style={{ border: 'none', boxShadow: 'none' }}>
+              <div
+                key={index}
+                className="col-md-3 mb-4"
+                onMouseEnter={() => setHoveredIndex(index)}
+                onMouseLeave={() => setHoveredIndex(null)}
+              >
+                <div
+                  className="card product-card"
+                  style={{
+                    border: 'none',
+                    boxShadow: 'none',
+                    position: 'relative',
+                    overflow: 'hidden',
+                  }}
+                >
                   <img src={product.img} className="card-img-top" alt={product.title} />
+
+                  {/* Nội dung chi tiết sản phẩm */}
                   <div className="card-body text-center">
                     <h6 className="card-title" style={{ fontSize: '14px' }}>{product.title}</h6>
                     <p className="card-text" style={{ fontWeight: 'bold', fontSize: '12px' }}>{product.price} đ</p>
                   </div>
+
+
+<div
+  className="product-actions"
+  style={{
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    opacity: hoveredIndex === index ? 1 : 0,
+    transition: 'opacity 0.3s ease',
+    display: 'flex', // Sử dụng flexbox để căn chỉnh
+    justifyContent: 'center', // Căn giữa các nút
+    gap: '10px',
+    color: '#fff',
+  }}
+>
+<a
+  className="btn wishlist awe-button product-quick-whistlist"
+  href="/account/login"
+  title="Add to wishlist"
+  style={{ color: '#fff', textDecoration: 'none' }}
+>
+  <i className="fas fa-heart" style={{ marginTop: '90px' }}></i>
+</a>
+<a style={{ marginTop: '90px' }}>
+  /
+</a>
+
+<a
+  href="/collections/available-now/products/linkin-park-one-more-light-di-a-cd"
+  className="btn product-quick-view btn-quickview"
+  title="Quickview"
+  style={{ color: '#fff', textDecoration: 'none' }}
+>
+  <i className="fas fa-eye" style={{ marginTop: '90px' }}></i>
+</a>
+
+</div>
+
                 </div>
               </div>
             ))}
