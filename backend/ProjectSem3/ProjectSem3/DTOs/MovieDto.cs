9de8ea090a608d5c544eea4b8a5cbd256d65b3a6
HungPham123123 <129242472+HungPham123123@users.noreply.github.com>
2024-11-15 11:10:21 +0700

    hi

--- backend/ProjectSem3/ProjectSem3/DTOs/MovieDto.cs
+++ backend/ProjectSem3/ProjectSem3/DTOs/MovieDto.cs
@@ -6,7 +6,6 @@
         public int? DirectorId { get; set; }
         public int? ProducerId { get; set; }
         public string? Biography { get; set; }
-        public string? Link { get; set; }
         public DateTime? CreatedAt { get; set; }
         public DateTime? UpdatedAt { get; set; }
     }
